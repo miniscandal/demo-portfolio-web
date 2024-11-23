69d0c3552aa1f3e89f3fea28130600d2289631cc
miniscandal <oscar01dev@gmail.com>
2024-11-23 17:05:04 -0600

    feat: add new icon svg

--- src/shared/components/atoms/icon-font-svg/variants.jsx
+++ src/shared/components/atoms/icon-font-svg/variants.jsx
@@ -55,7 +55,7 @@ const ArrowBack = (
 );
 
 const ArrowDown = (
-    <svg viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M10 8L14 8V10L8 16L2 10V8H6V0L10 4.76995e-08V8Z" fill="currentColor"></path> </g></svg>
+    <svg viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier"></g><g id="SVGRepo_tracerCarrier"></g><g id="SVGRepo_iconCarrier"> <path d="M10 8L14 8V10L8 16L2 10V8H6V0L10 4.76995e-08V8Z" fill="currentColor"></path> </g></svg>
 );
 
 
