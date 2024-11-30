3ac676772e842852b192b84e1d71b196ff149c12
miniscandal <oscar01dev@gmail.com>
2024-11-29 22:04:38 -0600

    feat: add new pixel art items

--- src/shared/components/molecules/pixel-art/variants.js
+++ src/shared/components/molecules/pixel-art/variants.js
@@ -3,13 +3,15 @@ const PIXEL_ART_ANIME_UMI = 'anime-umi';
 const PIXEL_ART_ANIME_RINTARO = 'anime-rintaro';
 const PIXEL_ART_ANIME_TODOROKI = 'anime-todoroki';
 const PIXEL_ART_ANIME_HINATA = 'anime-hinata';
+const PIXEL_ART_ANIME_KURISU = 'anime-kurisu';
+const PIXEL_ART_ANIME_KANAO = 'anime-kanao';
+const PIXEL_ART_ANIME_KOMI = 'anime-komi';
 const PIXEL_ART_ITEM_DESK = 'item-desk';
 const PIXEL_ART_ITEM_WHITEBOARD = 'item-whiteboard';
 const PIXEL_ART_ITEM_COMPUTERS_STEINS_GATE = 'item-computers-steins-gate';
 const PIXEL_ART_ITEM_MICROWAVE_STEINS_GATE = 'item-microwave-steins-gate';
-const PIXEL_ART_ANIME_KURISU = 'anime-kurisu';
-const PIXEL_ART_ANIME_KANAO = 'anime-kanao';
-const PIXEL_ART_ANIME_KOMI = 'anime-komi';
+const PIXEL_ART_ITEM_STICKY_NOTE_SINGLE = 'item-sticky-note-single';
+const PIXEL_ART_ITEM_STICKY_NOTE_GROUP = 'item-sticky-note-group';
 
 export {
     PIXEL_ART_HAMTARO,
@@ -23,5 +25,7 @@ export {
     PIXEL_ART_ITEM_MICROWAVE_STEINS_GATE,
     PIXEL_ART_ANIME_KURISU,
     PIXEL_ART_ANIME_KANAO,
-    PIXEL_ART_ANIME_KOMI
+    PIXEL_ART_ANIME_KOMI,
+    PIXEL_ART_ITEM_STICKY_NOTE_SINGLE,
+    PIXEL_ART_ITEM_STICKY_NOTE_GROUP
 };
