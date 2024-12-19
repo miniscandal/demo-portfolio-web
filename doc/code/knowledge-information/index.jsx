90f9fbb502c2e656454b0963a0102bf6275d89a0
miniscandal <oscar01dev@gmail.com>
2024-12-19 07:28:15 -0600

    feat: update css style

--- doc/code/knowledge-information/index.jsx
+++ doc/code/knowledge-information/index.jsx
@@ -2,6 +2,7 @@ import { PixelArt } from '@shared-molecules/pixel-art';
 import { Paragraph } from '@shared-atoms/paragraph';
 
 import { PIXEL_ART_ITEM_BOOK } from '@shared-molecules/pixel-art/variants';
+import { PIXEL_ART_ITEM_BOOKS } from '@shared-molecules/pixel-art/variants';
 
 import './style.css';
 
@@ -10,6 +11,10 @@ function KnowledgeInformation({
     name: text = 'Knowledge Information',
     color: bgColor = 'lightpink',
 }) {
+    const pixelArtBooks = {
+        size: 'small',
+        character: PIXEL_ART_ITEM_BOOKS
+    };
     const pixelArtBook = {
         size: 'small',
         character: PIXEL_ART_ITEM_BOOK
@@ -21,9 +26,7 @@ function KnowledgeInformation({
     return (
         <div className='knowledge-information' style={{ backgroundColor: bgColor }}>
             <Paragraph {...paragraph} />
-            <div>
-                <PixelArt {...pixelArtBook} />
-            </div>
+            <PixelArt {...pixelArtBook} />
         </div>
     );
 }
