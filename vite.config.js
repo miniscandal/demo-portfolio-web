ab5844862d7c90a4a350cdff974cda1ce72ff38c
miniscandal <oscar01dev@gmail.com>
2024-12-20 18:59:56 -0600

    chore: add new path and new alias

--- vite.config.js
+++ vite.config.js
@@ -31,11 +31,15 @@ export default defineConfig({
 
             '@feat-primary-footer-molecules': '/src/features/primary-footer/components/molecules/',
             '@feat-primary-footer-organisms': '/src/features/primary-footer/components/organisms/',
-            '@feat-primary-footer-pages': '/src/features/primary-footer/components/pages/',
+            '@feat-primary-footer-templates': '/src/features/primary-footer/components/templates/',
 
             '@feat-professional-profile-organisms': '/src/features/professional-profile/components/organisms/',
             '@feat-professional-profile-templates': '/src/features/professional-profile/components/templates/',
             '@feat-professional-profile-contexts': '/src/features/professional-profile/contexts/',
+
+            '@feat-about-me-organisms': '/src/features/about-me/components/organisms/',
+            '@feat-about-me-templates': '/src/features/about-me/components/templates/',
+            '@feat-about-me-contexts': '/src/features/about-me/contexts/',
 
             '@feat-personal-software-projects-molecules': '/src/features/personal-software-projects/components/molecules/',
             '@feat-personal-software-projects-organisms': '/src/features/personal-software-projects/components/organisms/',
