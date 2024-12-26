e76bba723b9acc0d9fa2bedd640eed1b317af6b4
miniscandal <oscar01dev@gmail.com>
2024-12-26 02:57:37 -0600

    refactor: modify pixel art character

--- src/features/about-me/components/organisms/professional-approach/index.jsx
+++ src/features/about-me/components/organisms/professional-approach/index.jsx
@@ -2,6 +2,9 @@ import { Title } from '@shared-atoms/title';
 import { PixelArt } from '@shared-molecules/pixel-art';
 import { Paragraph } from '@shared-atoms/paragraph';
 
+import { PIXEL_ART_ANIME_MINICODE_ANIMATION_V1 } from '@shared-molecules/pixel-art/variants';
+import { PIXEL_ART_ANIME_MINICODE_ANIMATION_V2 } from '@shared-molecules/pixel-art/variants';
+
 import './style.css';
 
 
@@ -16,13 +19,15 @@ function ProfessionalApproach() {
     const paragraphTwo = {
         text: `Deseo ser una fuente de inspiración y un mentor para el equipo de ingenieros en formación. Creo que al hacer esto, no solo avanzaré en mi carrera profesional sino que también enriqueceré aspectos importantes de mi vida.`
     };
-
+    const pixelArt = {
+        character: PIXEL_ART_ANIME_MINICODE_ANIMATION_V2
+    };
 
     return (
         <section className='professional-approach'>
             <Title {...title} />
             <div className='professional-approach__div'>
-                <PixelArt />
+                <PixelArt {...pixelArt} />
                 <div>
                     <Paragraph {...paragraphOne} />
                     <Paragraph {...paragraphTwo} />
