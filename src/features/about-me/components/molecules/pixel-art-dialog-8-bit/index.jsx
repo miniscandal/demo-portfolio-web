ba6a0bf79479322cfad56aa3bd3e435da648fa65
miniscandal <oscar01dev@gmail.com>
2024-12-26 06:34:51 -0600

    feat: add new web component

---
+++ src/features/about-me/components/molecules/pixel-art-dialog-8-bit/index.jsx
@@ -0,0 +1,45 @@
+import { PixelArt } from '@shared-molecules/pixel-art';
+import { Paragraph } from '@shared-atoms/paragraph';
+
+import { PIXEL_ART_ITEM_DIALOG } from '@shared-molecules/pixel-art/variants';
+import { PIXEL_ART_ANIME_UMI } from '@shared-molecules/pixel-art/variants';
+
+import './style.css';
+
+
+function PixelArtDialog8Bit({
+    textMain = 'pixel art dialog 8 bit',
+    textSecondary = 'pixel art dialog 8 bit',
+    character = PIXEL_ART_ANIME_UMI
+}) {
+    const paragraphOne = {
+        text: textMain
+    };
+    const paragraphTwo = {
+        text: textSecondary
+    };
+    const pixelArtCharacter = {
+        character
+    };
+    const pixelArtItem = {
+        character: PIXEL_ART_ITEM_DIALOG
+    };
+
+
+    return (
+        <section className='pixel-art-dialog-8-bit'>
+            <div className='pixel-art-dialog-8-bit__div'>
+                <PixelArt {...pixelArtCharacter} />
+            </div>
+            <div className='pixel-art-dialog-8-bit__div'>
+                <PixelArt {...pixelArtItem} />
+                <div className='pixel-art-dialog-8-bit__div--paragraph'>
+                    <Paragraph {...paragraphOne} />
+                    <Paragraph {...paragraphTwo} />
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export { PixelArtDialog8Bit };
