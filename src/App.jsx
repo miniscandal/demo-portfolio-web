1df33717381e91b0726e297a852c6de0d5dca71b
miniscandal <oscar01dev@gmail.com>
2024-12-27 04:19:55 -0600

    refactor: update css style and html structure

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
 
     return (
         <>
-            <HeaderContent />
+            <PrimaryHeader />
             {/*
             <scroll-container>
                 <PrimaryHeader />
