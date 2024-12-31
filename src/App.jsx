a07e206f4f0de07efcd9ce791959dfac8c992ba3
miniscandal <oscar01dev@gmail.com>
2024-12-31 08:04:38 -0600

    refactor: modify pixel art characters

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,20 @@ import { ProfessionalProfile } from '@feat-professional-profile-templates/index'
 import { AboutMe } from '@feat-about-me-templates/index';
 
 import './App.css';
-
+import { PixelArt } from '@shared-molecules/pixel-art';
+import { PIXEL_ART_ANIME_ITSUKI_NAKANO } from '@shared-molecules/pixel-art/variants';
+import { PIXEL_ART_ANIME_ICHIKA_NAKANO } from '@shared-molecules/pixel-art/variants';
+import { PIXEL_ART_ANIME_NINO_NAKANO } from '@shared-molecules/pixel-art/variants';
+import { PIXEL_ART_ANIME_YOTSUBA_NAKANO } from '@shared-molecules/pixel-art/variants';
+import { PIXEL_ART_ANIME_MIKU_NAKANO } from '@shared-molecules/pixel-art/variants';
 
 function App() {
 
     return (
         <>
+            <PixelArt character={PIXEL_ART_ANIME_MIKU_NAKANO} />
             <scroll-container>
-                <PrimaryHeader />
+                {/* <PrimaryHeader /> */}
                 <main>
                     <scroll-page>
                         <div>
