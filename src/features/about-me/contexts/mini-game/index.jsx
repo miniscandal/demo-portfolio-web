2e1441a5e6bc6d7f4fd1f32733d0afaf3aec7152
miniscandal <oscar01dev@gmail.com>
2025-01-18 12:51:37 -0600

    refactor: modify css style, programming logic and html structure

--- src/features/about-me/contexts/mini-game/index.jsx
+++ src/features/about-me/contexts/mini-game/index.jsx
@@ -1,48 +1,54 @@
 import { createContext } from 'react';
 import { useState } from 'react';
 
-import { STEP_PLAYER_CENTER_STEP } from '@feat-about-me-molecules/step-game/variants';
-import { STEP_PLAYER_RED_STEP } from '@feat-about-me-molecules/step-game/variants';
-import { STEP_PLAYER_BLUE_STEP } from '@feat-about-me-molecules/step-game/variants';
+import { STEP_GAME_CENTER_STEP } from '@feat-about-me-molecules/step-game/variants';
+import { STEP_GAME_RED_STEP } from '@feat-about-me-molecules/step-game/variants';
+import { STEP_GAME_BLUE_STEP } from '@feat-about-me-molecules/step-game/variants';
 
 
 const MiniGameContext = createContext({
-    stateEndGame: {},
-    stateSequenceGame: {}
+    gameState: {
+        sequenceGame: [{}],
+        endGame: undefined
+    },
+    setGameState: undefined
 });
 
 
 function MiniGameProvider({ children }) {
-    const [endGame, setEndGame] = useState(false);
-    const [sequenceGame, setSequenceGame] = useState([
-        {
-            type: STEP_PLAYER_RED_STEP, orientationLeft: true
-        },
-        {
-            type: STEP_PLAYER_CENTER_STEP
-        },
-        {
-            type: STEP_PLAYER_BLUE_STEP, orientationLeft: true
-        },
-        {
-            type: STEP_PLAYER_RED_STEP
-        },
-        {
-            type: STEP_PLAYER_CENTER_STEP
-        },
-        {
-            type: STEP_PLAYER_BLUE_STEP
-        }
-    ]);
+    const [gameState, setGameState] = useState({
+        sequenceGame: [
+            {
+                id: '0',
+                type: STEP_GAME_RED_STEP, orientationLeft: true
+            },
+            {
+                id: '1',
+                type: STEP_GAME_CENTER_STEP
+            },
+            {
+                id: '2',
+                type: STEP_GAME_BLUE_STEP, orientationLeft: true
+            },
+            {
+                id: '3',
+                type: STEP_GAME_RED_STEP
+            },
+            {
+                id: '4',
+                type: STEP_GAME_CENTER_STEP
+            },
+            {
+                id: '5',
+                type: STEP_GAME_BLUE_STEP
+            }
+        ],
+        endGame: false
+    });
+
     const provider = {
-        stateEndGame: {
-            endGame,
-            setEndGame
-        },
-        stateSequenceGame: {
-            sequenceGame,
-            setSequenceGame
-        }
+        gameState,
+        setGameState
     };
 
 
