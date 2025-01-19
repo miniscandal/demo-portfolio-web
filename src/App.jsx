4e8374a57da2a06da9cea3103085ba92ce1e2840
miniscandal <oscar01dev@gmail.com>
2025-01-18 19:38:35 -0600

    refactor: modify css style and html structure

--- src/App.jsx
+++ src/App.jsx
@@ -5,37 +5,55 @@ import { PersonalSoftwareProjects } from '@feat-personal-software-projects-pages
 import { ProfessionalProfile } from '@feat-professional-profile-templates/index';
 import { AboutMe } from '@feat-about-me-templates/index';
 
+import { SCROLL_PAGE_WORK_EXPERIENCE_HISTORY } from '@shared-constants/anchor-id';
+import { SCROLL_PAGE_PERSONAL_SOFTWARE_PROJECTS } from '@shared-constants/anchor-id';
+import { SCROLL_PAGE_PROFESSIONAL_PROFILE } from '@shared-constants/anchor-id';
+import { SCROLL_PAGE_ABOUT_ME } from '@shared-constants/anchor-id';
+import { SCROLL_PAGE_PRIMARY_FOOTER } from '@shared-constants/anchor-id';
+
 import './App.css';
+
+import { MainSection } from '@feat-about-me-organisms/main-section';
+import { DescriptiveCard } from '@feat-about-me-organisms/descriptive-card';
+import { Header } from '@feat-about-me-organisms/header';
+import { MainSectionRightSide } from '@feat-about-me-organisms/main-section-right-side';
+import { AboutMeProvider } from '@feat-about-me-contexts/about-me';
+import { MiniGameProvider } from '@feat-about-me-contexts/mini-game';
+import { MiniGame } from '@feat-about-me-organisms/mini-game';
 
 function App() {
 
     return (
         <>
+            <PrimaryHeader />
             <scroll-container>
-                <PrimaryHeader />
                 <main>
-                    <scroll-page>
-                        <div>
+                    <scroll-page id={SCROLL_PAGE_WORK_EXPERIENCE_HISTORY}>
+                        <div className='main__div'>
                             <WorkExperienceHistory />
                         </div>
                     </scroll-page>
-                    <scroll-page>
-                        <div>
+                    <scroll-page id={SCROLL_PAGE_PERSONAL_SOFTWARE_PROJECTS}>
+                        <div className='main__div'>
                             <PersonalSoftwareProjects />
                         </div>
                     </scroll-page>
-                    <scroll-page>
-                        <div>
+                    <scroll-page id={SCROLL_PAGE_PROFESSIONAL_PROFILE}>
+                        <div className='main__div'>
                             <ProfessionalProfile />
                         </div>
                     </scroll-page>
-                    <scroll-page>
-                        <div>
+                    <scroll-page id={SCROLL_PAGE_ABOUT_ME}>
+                        <div className='main__div'>
                             <AboutMe />
                         </div>
                     </scroll-page>
                 </main>
-                <PrimaryFooter />
+                <scroll-page id={SCROLL_PAGE_PRIMARY_FOOTER}>
+                    <div className='main__div'>
+                        <PrimaryFooter />
+                    </div>
+                </scroll-page>
             </scroll-container>
         </>
     );
