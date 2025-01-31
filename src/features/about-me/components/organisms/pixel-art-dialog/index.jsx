1c2f9feed16630dff7a424d2728340189a582a61
miniscandal <oscar01dev@gmail.com>
2025-01-31 17:03:22 -0600

    feat: add new main components

--- src/features/about-me/components/organisms/pixel-art-dialog/index.jsx
+++ src/features/about-me/components/organisms/pixel-art-dialog/index.jsx
@@ -1,6 +1,6 @@
-import { PixelArt } from '@shared-atoms/pixel-art';
-
 import { SliderDialog } from '@feat-about-me-molecules/slider-pixel-art-dialog';
+
+import { LeadershipPixelArtObjects } from '@feat-about-me-molecules/leadership-pixel-art-objects';
 
 import { PIXEL_ART_ANIME_MINICODE_ANIMATION_V2 } from '@shared-atoms/pixel-art/variants';
 
@@ -10,14 +10,18 @@ import './style.css';
 function PixelArtDialog({
     textMain = 'Pixel Art Dialog Main',
     textSecondary = 'Pixel Art Dialog Secondary',
-    character = PIXEL_ART_ANIME_MINICODE_ANIMATION_V2
+    character = PIXEL_ART_ANIME_MINICODE_ANIMATION_V2,
+    objects = [],
+    type
 }) {
     const sliderDialog = {
         textMain,
-        textSecondary
+        textSecondary,
     };
-    const pixelArtCharacter = {
-        character
+    const leadershipPixelArtObjects = {
+        character,
+        objects,
+        type
     };
 
 
@@ -26,9 +30,7 @@ function PixelArtDialog({
             <div className='pixel-art-dialog__div'>
                 <SliderDialog {...sliderDialog} />
             </div>
-            <div className='pixel-art-dialog__div'>
-                <PixelArt {...pixelArtCharacter} />
-            </div>
+            <LeadershipPixelArtObjects {...leadershipPixelArtObjects} />
         </section>
     );
 }
---
+++ src/features/about-me/components/organisms/self-description/index.jsx
@@ -0,0 +1,50 @@
+import { PixelArtDialog } from '@feat-about-me-organisms/pixel-art-dialog';
+
+import { Title } from '@shared-atoms/title';
+import { PixelArt } from '@shared-atoms/pixel-art';
+
+import { PIXEL_ART_ITEM_CUP_COFFEE } from '@shared-atoms/pixel-art/variants';
+import { PIXEL_ART_ANIME_MINICODE_ANIMATION_V3 } from '@shared-atoms/pixel-art/variants';
+
+import './style.css';
+
+
+function SelfDescription({
+    titleText = 'Self Description',
+    dialogTexts = ['Main Self Description', 'Secondary Self Description'],
+    pixelArtCharacter = PIXEL_ART_ANIME_MINICODE_ANIMATION_V3,
+    pixelArtObjects = [
+        () => <PixelArt character={PIXEL_ART_ITEM_CUP_COFFEE} />,
+        () => <PixelArt character={PIXEL_ART_ITEM_CUP_COFFEE} />,
+        () => <PixelArt character={PIXEL_ART_ITEM_CUP_COFFEE} />,
+        () => <PixelArt character={PIXEL_ART_ITEM_CUP_COFFEE} />,
+        () => <PixelArt character={PIXEL_ART_ITEM_CUP_COFFEE} />
+    ],
+    type = 'rectangular-figure'
+}) {
+    const title = {
+        text: titleText,
+        type: 'h3',
+        color: 'light'
+    };
+
+    const pixelArtDialog = {
+        textMain: dialogTexts[0],
+        textSecondary: dialogTexts[1],
+        character: pixelArtCharacter,
+        objects: pixelArtObjects,
+        type
+    };
+
+
+    return (
+        <section className='self-description'>
+            <div className='self-description__div'>
+                <Title {...title} />
+            </div>
+            <PixelArtDialog {...pixelArtDialog} />
+        </section>
+    );
+}
+
+export { SelfDescription };
