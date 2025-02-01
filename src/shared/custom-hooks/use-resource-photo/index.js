f66e15aaa38f49385b684ae85ddbec4688f98523
miniscandal <oscar01dev@gmail.com>
2025-02-01 16:07:11 -0600

    refactor: modify css code and html structure

--- src/shared/custom-hooks/use-resource-photo/index.js
+++ src/shared/custom-hooks/use-resource-photo/index.js
@@ -3,7 +3,6 @@ import { use } from 'react';
 import { resourcePersonalPhotos } from '@core-services-api-rest-github-profile/data/resources-personal-photos';
 
 
-
 function useResourcePhoto() {
     const { promise } = resourcePersonalPhotos();
     const data = use(promise);
