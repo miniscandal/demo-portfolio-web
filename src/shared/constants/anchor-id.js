c7c1be740050a72680fc8b0166d8d929d3b24d2b
miniscandal <oscar01dev@gmail.com>
2025-02-14 18:45:56 -0600

    feat: add new constants

--- src/shared/constants/anchor-id.js
+++ src/shared/constants/anchor-id.js
@@ -1,8 +1,11 @@
+// These constants are used to refer to specific sections of the page for easy navigation and scrolling
+
 const SCROLL_PAGE_WORK_EXPERIENCE_HISTORY = 'work-experience-history';
 const SCROLL_PAGE_ABOUT_ME = 'about-me';
 const SCROLL_PAGE_PERSONAL_SOFTWARE_PROJECTS = 'personal-software-projects';
 const SCROLL_PAGE_PROFESSIONAL_PROFILE = 'professional-profile';
 const SCROLL_PAGE_PRIMARY_FOOTER = 'primary-footer';
+
 
 export {
     SCROLL_PAGE_WORK_EXPERIENCE_HISTORY,
