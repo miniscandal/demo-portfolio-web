b930460ce10c6b820afb2e37c40e8e5590617173
miniscandal <oscar01dev@gmail.com>
2025-02-26 23:22:37 -0600

    feat: add new constants

---
+++ src/features/personal-software-projects/constants/manga-panel-id.js
@@ -0,0 +1,26 @@
+/**
+ * Constants representing unique identifiers for different sections or panels of a Manga Page.
+ * 
+ * Prefixes:
+ * - "MPP_" → Represents the main sections of a Manga Page.
+ * - "MPS_" → Represents secondary sections within a Manga Page.
+ */
+
+const MPP_DESCRIPTION_ID = 'mpp-description-id';
+const MPP_PLANNING_ID = 'mpp-planning-id';
+const MPP_TECH_ECOSYSTEM_ID = 'mpp-tech-ecosystem-id';
+const MPP_PREVIEW_ID = 'mpp-preview-id';
+
+const MPS_MAIN_ID = 'mps-main-id';
+const MPS_PRIMARY_ID = 'mps-primary-id';
+const MPS_SECONDARY_ID = 'mps-secondary-id';
+
+export {
+    MPP_DESCRIPTION_ID,
+    MPP_PLANNING_ID,
+    MPP_TECH_ECOSYSTEM_ID,
+    MPP_PREVIEW_ID,
+    MPS_MAIN_ID,
+    MPS_PRIMARY_ID,
+    MPS_SECONDARY_ID
+};
