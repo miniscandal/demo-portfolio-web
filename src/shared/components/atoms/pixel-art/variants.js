6d749331c00d88a90947b07a47d1046a108f7e19
miniscandal <oscar01dev@gmail.com>
2025-02-28 06:28:02 -0600

    refactor: organize css classes and constants

--- src/shared/components/atoms/pixel-art/variants.js
+++ src/shared/components/atoms/pixel-art/variants.js
@@ -1,3 +1,95 @@
+const PIXEL_ART_ITEM_ANIME_DANGO_BLUE = 'item-anime-dango-blue';
+const PIXEL_ART_ITEM_ANIME_DANGO_PINK = 'item-anime-dango-pink';
+const PIXEL_ART_ITEM_ANIME_DANGO_YELLOW = 'item-anime-dango-yellow';
+
+const PIXEL_ART_ITEM_ANIME_HITAI_ATE = 'item-anime-hitai-ate';
+const PIXEL_ART_ITEM_ANIME_PARCHMENT = 'item-anime-parchment';
+
+const PIXEL_ART_ITEM_ANIME_STEINS_GATE_COMPUTERS = 'item-anime-steins-gate-computers';
+const PIXEL_ART_ITEM_ANIME_STEINS_GATE_MICROWAVE = 'item-anime-steins-gate-microwave';
+
+const PIXEL_ART_ITEM_ARCADE = 'item-arcade';
+
+const PIXEL_ART_ITEM_BOOK_UNIVERSITY = 'item-book-university';
+const PIXEL_ART_ITEM_BOOK = 'item-book';
+const PIXEL_ART_ITEM_BOOKS = 'item-books';
+
+const PIXEL_ART_ITEM_CUP_COFFEE_ANIMATION = 'item-cup-coffee-animation';
+
+const PIXEL_ART_ITEM_DESK_ANIMATION = 'item-desk-animation';
+const PIXEL_ART_ITEM_DESK_INVERT_ANIMATION = 'item-desk-invert-animation';
+
+const PIXEL_ART_ITEM_DOMO_ANIMATION = 'item-domo-animation';
+const PIXEL_ART_ITEM_DOMO = 'item-domo';
+
+const PIXEL_ART_ITEM_HOURGLASS = 'item-hourglass';
+
+const PIXEL_ART_ITEM_LAMP = 'item-lamp';
+
+const PIXEL_ART_ITEM_MORTARBOARD = 'item-mortarboard';
+
+const PIXEL_ART_ITEM_PENCIL_BLUE = 'item-pencil-blue';
+const PIXEL_ART_ITEM_PENCIL_PINK = 'item-pencil-pink';
+const PIXEL_ART_ITEM_PENCIL_RED = 'item-pencil-red';
+const PIXEL_ART_ITEM_PENCIL_YELLOW = 'item-pencil-yellow';
+
+const PIXEL_ART_ITEM_PUMP_GAME_LOGO = 'item-pump-game-logo';
+const PIXEL_ART_ITEM_PUMP_STEP_BLUE_ANIMATION = 'item-pump-step-blue-animation';
+const PIXEL_ART_ITEM_PUMP_STEP_BLUE = 'item-pump-step-blue';
+const PIXEL_ART_ITEM_PUMP_STEP_CENTER_ANIMATION = 'item-pump-step-center-animation';
+const PIXEL_ART_ITEM_PUMP_STEP_CENTER = 'item-pump-step-center';
+const PIXEL_ART_ITEM_PUMP_STEP_RED_ANIMATION = 'item-pump-step-red-animation';
+const PIXEL_ART_ITEM_PUMP_STEP_RED = 'item-pump-step-red';
+
+const PIXEL_ART_ITEM_SOCCER_BALL = 'item-soccer-ball';
+
+const PIXEL_ART_ITEM_STICKY_NOTE_GROUP = 'item-sticky-note-group';
+const PIXEL_ART_ITEM_STICKY_NOTE = 'item-sticky-note';
+
+const PIXEL_ART_ITEM_WHITEBOARD = 'item-whiteboard';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const PIXEL_ART_ITEM_SPEECH_BUBBLE_BIT_V2 = 'item-speech-bubble-bit-v2';
+
+const PIXEL_ART_ITEM_SPEECH_BUBBLE_BIT = 'item-speech-bubble-bit';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const PIXEL_ART_ANIME_UMI = 'anime-umi';
 const PIXEL_ART_ANIME_RINTARO = 'anime-rintaro';
 const PIXEL_ART_ANIME_TODOROKI = 'anime-todoroki';
@@ -36,45 +128,37 @@ const PIXEL_ART_ANIME_MINICODE_ANIMATION_V5 = 'anime-minicode-animation-v5';
 const PIXEL_ART_ANIME_MINICODE_BASIC_V1 = 'anime-minicode-basic-v1';
 const PIXEL_ART_ANIME_MINICODE_BASIC_V2 = 'anime-minicode-basic-v2';
 
-const PIXEL_ART_ITEM_DESK_ANIMATION = 'item-desk-animation';
-const PIXEL_ART_ITEM_DESK_INVERT_ANIMATION = 'item-desk-invert-animation';
-const PIXEL_ART_ITEM_WHITEBOARD = 'item-whiteboard';
-const PIXEL_ART_ITEM_COMPUTERS_STEINS_GATE = 'item-computers-steins-gate';
-const PIXEL_ART_ITEM_MICROWAVE_STEINS_GATE = 'item-microwave-steins-gate';
-const PIXEL_ART_ITEM_STICKY_NOTE_SINGLE = 'item-sticky-note-single';
-const PIXEL_ART_ITEM_STICKY_NOTE_GROUP = 'item-sticky-note-group';
-const PIXEL_ART_ITEM_BOOK = 'item-book';
-const PIXEL_ART_ITEM_BOOKS = 'item-books';
-const PIXEL_ART_ITEM_DIALOG_VERTICAL = 'item-dialog-vertical';
-const PIXEL_ART_ITEM_HITAI_ATE = 'item-hitai-ate';
-const PIXEL_ART_ITEM_PARCHMENT = 'item-parchment';
-const PIXEL_ART_ITEM_SEALED_PARCHMENT = 'item-sealed-parchment';
-const PIXEL_ART_ITEM_BLUE_STEP = 'item-blue-step';
-const PIXEL_ART_ITEM_RED_STEP = 'item-red-step';
-const PIXEL_ART_ITEM_CENTER_STEP = 'item-center-step';
-const PIXEL_ART_ITEM_MORTARBOARD = 'item-mortarboard';
-const PIXEL_ART_ITEM_BOOK_UNIVERSITY = 'item-book-university';
-const PIXEL_ART_ITEM_HOURGLASS = 'item-hourglass';
-const PIXEL_ART_ITEM_DESK_LAMP = 'item-desk-lamp';
-const PIXEL_ART_ITEM_CUP_COFFEE = 'item-cup-coffee';
-const PIXEL_ART_ITEM_PENCIL_BLUE = 'item-pencil-blue';
-const PIXEL_ART_ITEM_PENCIL_RED = 'item-pencil-red';
-const PIXEL_ART_ITEM_PENCIL_YELLOW = 'item-pencil-yellow';
-const PIXEL_ART_ITEM_PENCIL_PINK = 'item-pencil-pink';
-const PIXEL_ART_ITEM_ARCADE = 'item-arcade';
-const PIXEL_ART_ITEM_SOCCER = 'item-soccer';
-const PIXEL_ART_ITEM_DOMO = 'item-domo';
-const PIXEL_ART_ITEM_DOMO_ANIMATION = 'item-domo-animation';
-const PIXEL_ART_ITEM_PUMP_GAME = 'item-pump-game';
-const PIXEL_ART_ITEM_PUMP_GAME_BLUE_STEP = 'item-pump-game-blue-step';
-const PIXEL_ART_ITEM_PUMP_GAME_RED_STEP = 'item-pump-game-red-step';
-const PIXEL_ART_ITEM_PUMP_GAME_CENTER_STEP = 'item-pump-game-center-step';
-const PIXEL_ART_ITEM_DANGO_BLUE = 'item-dango-blue';
-const PIXEL_ART_ITEM_DANGO_PINK = 'item-dango-pink';
-const PIXEL_ART_ITEM_DANGO_YELLOW = 'item-dango-yellow';
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 export {
+    PIXEL_ART_ITEM_ARCADE,
+    PIXEL_ART_ITEM_PUMP_STEP_BLUE,
+    PIXEL_ART_ITEM_PUMP_STEP_RED,
+    PIXEL_ART_ITEM_PUMP_STEP_CENTER,
+
+
+
+
+
+
+
+
+
+
+
+
     PIXEL_ART_ANIME_UMI,
     PIXEL_ART_ANIME_RINTARO,
     PIXEL_ART_ANIME_TODOROKI,
@@ -113,39 +197,35 @@ export {
     PIXEL_ART_ITEM_DESK_ANIMATION,
     PIXEL_ART_ITEM_DESK_INVERT_ANIMATION,
     PIXEL_ART_ITEM_WHITEBOARD,
-    PIXEL_ART_ITEM_COMPUTERS_STEINS_GATE,
-    PIXEL_ART_ITEM_MICROWAVE_STEINS_GATE,
-    PIXEL_ART_ITEM_STICKY_NOTE_SINGLE,
+    PIXEL_ART_ITEM_ANIME_STEINS_GATE_COMPUTERS,
+    PIXEL_ART_ITEM_ANIME_STEINS_GATE_MICROWAVE,
+    PIXEL_ART_ITEM_STICKY_NOTE,
     PIXEL_ART_ITEM_STICKY_NOTE_GROUP,
     PIXEL_ART_ITEM_BOOK,
     PIXEL_ART_ITEM_BOOKS,
-    PIXEL_ART_ITEM_DIALOG_VERTICAL,
-    PIXEL_ART_ITEM_HITAI_ATE,
-    PIXEL_ART_ITEM_PARCHMENT,
-    PIXEL_ART_ITEM_SEALED_PARCHMENT,
-    PIXEL_ART_ITEM_BLUE_STEP,
-    PIXEL_ART_ITEM_RED_STEP,
-    PIXEL_ART_ITEM_CENTER_STEP,
+    PIXEL_ART_ITEM_SPEECH_BUBBLE_BIT,
+    PIXEL_ART_ITEM_ANIME_HITAI_ATE,
+    PIXEL_ART_ITEM_ANIME_PARCHMENT,
     PIXEL_ART_ITEM_MORTARBOARD,
     PIXEL_ART_ITEM_BOOK_UNIVERSITY,
     PIXEL_ART_ITEM_HOURGLASS,
-    PIXEL_ART_ITEM_DESK_LAMP,
-    PIXEL_ART_ITEM_CUP_COFFEE,
+    PIXEL_ART_ITEM_LAMP,
+    PIXEL_ART_ITEM_CUP_COFFEE_ANIMATION,
     PIXEL_ART_ITEM_PENCIL_BLUE,
     PIXEL_ART_ITEM_PENCIL_RED,
     PIXEL_ART_ITEM_PENCIL_YELLOW,
     PIXEL_ART_ITEM_PENCIL_PINK,
-    PIXEL_ART_ITEM_ARCADE,
-    PIXEL_ART_ITEM_SOCCER,
+    PIXEL_ART_ITEM_SOCCER_BALL,
     PIXEL_ART_ITEM_DOMO,
     PIXEL_ART_ITEM_DOMO_ANIMATION,
-    PIXEL_ART_ITEM_PUMP_GAME,
-    PIXEL_ART_ITEM_PUMP_GAME_BLUE_STEP,
-    PIXEL_ART_ITEM_PUMP_GAME_RED_STEP,
-    PIXEL_ART_ITEM_PUMP_GAME_CENTER_STEP,
+    PIXEL_ART_ITEM_PUMP_GAME_LOGO,
+    PIXEL_ART_ITEM_PUMP_STEP_BLUE_ANIMATION,
+    PIXEL_ART_ITEM_PUMP_STEP_RED_ANIMATION,
+    PIXEL_ART_ITEM_PUMP_STEP_CENTER_ANIMATION,
     PIXEL_ART_ADVENTURE_TIME_JAKE_THE_DOG,
     PIXEL_ART_ADVENTURE_TIME_BMO,
-    PIXEL_ART_ITEM_DANGO_BLUE,
-    PIXEL_ART_ITEM_DANGO_PINK,
-    PIXEL_ART_ITEM_DANGO_YELLOW
+    PIXEL_ART_ITEM_ANIME_DANGO_BLUE,
+    PIXEL_ART_ITEM_ANIME_DANGO_PINK,
+    PIXEL_ART_ITEM_ANIME_DANGO_YELLOW,
+    PIXEL_ART_ITEM_SPEECH_BUBBLE_BIT_V2
 };
