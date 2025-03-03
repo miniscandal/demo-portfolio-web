80eedec3fb87356a8130d2952b233529df0cdf4f
miniscandal <oscar01dev@gmail.com>
2025-03-02 19:53:14 -0600

    refactor: simplify manga panels logic, rename props for clarity and adjust grid layout

--- src/features/personal-software-projects/components/organisms/manga-page-main/index.jsx
+++ src/features/personal-software-projects/components/organisms/manga-page-main/index.jsx
@@ -1,47 +1,21 @@
-import { MangaPanelProjectDescription } from '@feat-personal-software-projects-organisms/manga-panel-project-description';
-import { MangaPanelProjectPreview } from '@feat-personal-software-projects-organisms/manga-panel-project-preview';
-import { MangaPanelProjectAppliedConcepts } from '@feat-personal-software-projects-organisms/manga-panel-project-applied-concepts';
-import { MangaPanelProjectPlanning } from '@feat-personal-software-projects-organisms/manga-panel-project-planning';
-import { createMangaPanel } from '@feat-personal-software-projects-helpers/create-manga-panel';
-
-import {
-    MPP_DESCRIPTION_ID,
-    MPP_PLANNING_ID,
-    MPP_TECH_ECOSYSTEM_ID,
-    MPP_PREVIEW_ID,
-} from '@feat-personal-software-projects-constants/manga-panel-id';
-
-import { SelectableList } from '@shared-organisms/selectable-list';
+import { SelectableList } from '@shared-molecules/selectable-list';
 
 import './style.css';
 
 
-const MANGA_PANELS = [
-    {
-        id: MPP_DESCRIPTION_ID,
-        Component: createMangaPanel('Ep 01', <MangaPanelProjectDescription />)
-    },
-    {
-        id: MPP_PLANNING_ID,
-        Component: createMangaPanel('Ep 02', <MangaPanelProjectPlanning />)
-    },
-    {
-        id: MPP_TECH_ECOSYSTEM_ID,
-        Component: createMangaPanel('Ep 03', <MangaPanelProjectAppliedConcepts />)
-    },
-    {
-        id: MPP_PREVIEW_ID,
-        Component: createMangaPanel('Ep 04', <MangaPanelProjectPreview />)
-    }
-];
-
 function MangaPageMain({
-    onSelectPageId = () => { }
+    MangaPanels = [],
+    onSelectPanelId = () => { }
 }) {
+    const items = MangaPanels.map((value, index) => ({
+        ...value,
+        Component: <value.Component label={`Ep 0${index + 1}`} isMiniature={true} />
+    }));
+
 
     return (
         <div className='manga-page-main'>
-            <SelectableList items={MANGA_PANELS} onChange={onSelectPageId} />
+            <SelectableList items={items} onChange={onSelectPanelId} />
         </div>
     );
 }
