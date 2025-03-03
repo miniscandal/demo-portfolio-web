e623129a695c1ee6e3b066982dc116753426dd35
miniscandal <oscar01dev@gmail.com>
2025-03-02 20:00:07 -0600

    refactor: update component with new hooks and enhance interactivity, enhance layout and styling fot non-mimiature mode

--- src/features/personal-software-projects/components/organisms/manga-panel-expected-results/index.jsx
+++ src/features/personal-software-projects/components/organisms/manga-panel-expected-results/index.jsx
@@ -1,45 +1,57 @@
 import { useContext } from 'react';
 
+import { useIncreaseIndex } from '@shared-custom-hooks/use-increase-index/use-increase-index';
+
 import { MangaContext } from '@feat-personal-software-projects-contexts/manga-context';
 
+import { PixelArt } from '@shared-atoms/pixel-art';
 import { Title } from '@shared-atoms/title';
 
-import { PIXEL_ART_ANIME_STEINS_GATE_KURISU_ANIMATION } from '@shared-atoms/pixel-art/variants';
-import { PIXEL_ART_ITEM_ANIME_STEINS_GATE_COMPUTERS } from '@shared-atoms/pixel-art/variants';
-import { PixelArt } from '@shared-atoms/pixel-art';
+import { TypingDialogue } from '@shared-molecules/typing-dialogue';
+import { NextStepIndicator } from '@shared-molecules/next-step-indicator';
+import { HitBoxButton } from '@shared-molecules/hit-box-button';
+
+import {
+    PIXEL_ART_ANIME_STEINS_GATE_KURISU_ANIMATION,
+    PIXEL_ART_ITEM_ANIME_STEINS_GATE_COMPUTERS,
+    PIXEL_ART_ITEM_ANIME_STEINS_GATE_MICROWAVE
+} from '@shared-atoms/pixel-art/variants';
 
 import './style.css';
 
 
-function MangaPanelExpectedResults({ readingMode = false }) {
-    const { projectData: { expectedResults } } = useContext(MangaContext);
+function MangaPanelExpectedResults({ isMiniatureMode = false }) {
+    const { expectedResults } = useContext(MangaContext);
+    const { index, increaseIndex } = useIncreaseIndex({ resetLength: expectedResults.length });
+
     const classList = [
-        readingMode ? 'reading-mode' : ''
+        isMiniatureMode ? 'is-miniature-mode' : ''
     ];
     const title = {
         text: 'Expected Results',
         color: 'smoky-purple',
         type: 'h3'
     };
-    const pixelArt = {
-        character: PIXEL_ART_ITEM_ANIME_STEINS_GATE_COMPUTERS
-    };
+
 
     return (
         <section className={`manga-panel-expected-results ${classList.join(' ')}`}>
             <Title {...title} />
-            <section>
-                {
-                    readingMode
-                    &&
-                    <div className='character-speech__div'>
-                    </div>
-                }
-
-                <div className='pixel-art__div'>
-                    <PixelArt {...pixelArt} />
-                </div>
-            </section>
+            <TypingDialogue
+                message={expectedResults[index]}
+                character={PIXEL_ART_ANIME_STEINS_GATE_KURISU_ANIMATION}
+            />
+            {
+                !isMiniatureMode
+                &&
+                <>
+                    <PixelArt character={PIXEL_ART_ITEM_ANIME_STEINS_GATE_COMPUTERS} />
+                    <NextStepIndicator />
+                    <HitBoxButton handleClick={increaseIndex}>
+                        <PixelArt character={PIXEL_ART_ITEM_ANIME_STEINS_GATE_MICROWAVE} />
+                    </HitBoxButton>
+                </>
+            }
         </section>
     );
 }
