98e24a16a3fab518d9dd4bb2ba7a0168aa302fd0
miniscandal <oscar01dev@gmail.com>
2025-03-02 20:23:00 -0600

    feat: add new shared component as  molecule

---
+++ src/shared/components/molecules/typing-dialogue-segment-looping/index.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+
+import { useTypingEffectSegment } from '@shared-custom-hooks/use-typing-effect-segment';
+
+import { MangaDialogue } from '@shared-molecules/manga-dialogue';
+import { Paragraph } from '@shared-atoms/paragraph';
+
+import './style.css';
+
+
+function TypingDialogueSegmentLooping({
+    messages = ['Typing', 'Dialogue', 'Segment', 'Looping']
+}) {
+    const [indexText, setIndexText] = useState(0);
+    const [displayText, isTypingComplete, setIsTypingComplete] = useTypingEffectSegment({
+        id: messages[indexText].id,
+        message: messages[indexText].message
+    });
+
+    if (isTypingComplete) {
+        setIsTypingComplete(false);
+        setTimeout(() => {
+            setIndexText(prev => (prev === messages.length - 1) ? 0 : prev + 1);
+        }, 1000);
+    }
+
+
+    return (
+        <div className='typing-dialogue-segment-looping'>
+            <MangaDialogue size='regular'>
+                <Paragraph text={displayText} cursorAnimation={true} />
+            </MangaDialogue>
+        </div>
+    );
+}
+
+export { TypingDialogueSegmentLooping };
