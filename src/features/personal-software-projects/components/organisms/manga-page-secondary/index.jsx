1252c48527e61441e56d56f1bf89a8714acc5b33
miniscandal <oscar01dev@gmail.com>
2025-03-02 19:57:19 -0600

    refactor: panel handling, use and implement new component, adjust grid layout and add styles for checked items

--- src/features/personal-software-projects/components/organisms/manga-page-secondary/index.jsx
+++ src/features/personal-software-projects/components/organisms/manga-page-secondary/index.jsx
@@ -1,22 +1,16 @@
 import { useState } from 'react';
 
-import { MangaPanelProjectDescription } from '@feat-personal-software-projects-organisms/manga-panel-project-description';
 import { MangaPanelExpectedResults } from '@feat-personal-software-projects-organisms/manga-panel-expected-results';
 import { MangaPanelProjectPlanning } from '@feat-personal-software-projects-organisms/manga-panel-project-planning';
 import { MangaPanelProjectAppliedConcepts } from '@feat-personal-software-projects-organisms/manga-panel-project-applied-concepts';
 import { MangaPanelProjectPreview } from '@feat-personal-software-projects-organisms/manga-panel-project-preview';
+import { MangaPanel } from '@feat-personal-software-projects-molecules/manga-panel';
 
-import { SelectableList } from '@shared-organisms/selectable-list';
-
-import { createMangaPanel } from '@feat-personal-software-projects-helpers/create-manga-panel';
+import { SelectableList } from '@shared-molecules/selectable-list';
 
 import {
-    MPS_MAIN_ID,
     MPS_PRIMARY_ID,
-    MPS_SECONDARY_ID
-} from '@feat-personal-software-projects-constants/manga-panel-id';
-
-import {
+    MPS_SECONDARY_ID,
     MPP_DESCRIPTION_ID,
     MPP_PLANNING_ID,
     MPP_TECH_ECOSYSTEM_ID,
@@ -27,45 +21,43 @@ import './style.css';
 
 
 const MANGA_PANELS = {
-    [MPP_DESCRIPTION_ID]: [MangaPanelProjectDescription, MangaPanelExpectedResults],
-    [MPP_PLANNING_ID]: [MangaPanelProjectPlanning, () => { }],
-    [MPP_TECH_ECOSYSTEM_ID]: [MangaPanelProjectAppliedConcepts, () => { }],
-    [MPP_PREVIEW_ID]: [MangaPanelProjectPreview, () => { }]
+    [MPP_DESCRIPTION_ID]: MangaPanelExpectedResults,
+    [MPP_PLANNING_ID]: MangaPanelProjectPlanning,
+    [MPP_TECH_ECOSYSTEM_ID]: MangaPanelProjectAppliedConcepts,
+    [MPP_PREVIEW_ID]: MangaPanelProjectPreview
 };
 
-function MangaPageSecondary({ readMangaPageId }) {
+function MangaPageSecondary({
+    selectedMangaPanelId,
+    MainMangaPanel
+}) {
     const [readMangaPanelId, setReadMangaPanelId] = useState(MPS_PRIMARY_ID);
+
+    const SecondaryMangaPanel = MANGA_PANELS[selectedMangaPanelId];
+
+    const mainPanel = {
+        id: readMangaPanelId === MPS_PRIMARY_ID ? MPS_PRIMARY_ID : MPS_SECONDARY_ID,
+        Component: readMangaPanelId === MPS_PRIMARY_ID
+            ? <MainMangaPanel.Component label='Sc 01' isMiniatureMode={false} />
+            : <MangaPanel label='Sc 02'> <SecondaryMangaPanel isMiniatureMode={false} /></MangaPanel>
+    };
 
     const primaryPanel = {
         id: MPS_PRIMARY_ID,
-        Component: createMangaPanel('Sc 01', MANGA_PANELS[readMangaPageId][0]({
-            isModePreview: MPS_PRIMARY_ID === readMangaPageId
-        }))
+        Component: <MainMangaPanel.Component label='Sc 01' isMiniatureMode={true} />
     };
+
     const secondaryPanel = {
         id: MPS_SECONDARY_ID,
-        Component: createMangaPanel('Sc 02', MANGA_PANELS[readMangaPageId][0]({
-            isModePreview: MPS_SECONDARY_ID === readMangaPageId
-        }))
+        Component: <MangaPanel label='Sc 02'><SecondaryMangaPanel isMiniatureMode={true} /></MangaPanel>
     };
-
-    const mainPanel = (readMangaPanelId === MPS_PRIMARY_ID) ? primaryPanel : secondaryPanel;
-
-    const mangaPanels = [
-        {
-            ...mainPanel,
-            id: MPS_MAIN_ID
-        },
-        primaryPanel,
-        secondaryPanel
-    ];
 
 
     return (
         <div className='manga-page-secondary'>
             <SelectableList
                 defaultSelectItemId={MPS_PRIMARY_ID}
-                items={mangaPanels}
+                items={[mainPanel, primaryPanel, secondaryPanel]}
                 onChange={setReadMangaPanelId}
             />
         </div>
