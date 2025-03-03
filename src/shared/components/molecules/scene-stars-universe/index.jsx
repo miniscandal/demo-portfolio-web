700e73d835cb49bc4b6b8fd416a532e1b293c681
miniscandal <oscar01dev@gmail.com>
2025-03-02 20:22:47 -0600

    feat: add new shared component as  molecule

---
+++ src/shared/components/molecules/scene-stars-universe/index.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react';
+
+import './style.css';
+
+function SceneStarsUniverse() {
+    const [stars, setStars] = useState([]);
+
+    useEffect(() => {
+        const createStars = () => {
+            const stars = Array.from({ length: 200 }, (_, i) => ({
+                id: i,
+                x: Math.random() * 10,
+                y: Math.random() * 10,
+                delay: `${Math.random() * 1.5}s`,
+            }));
+
+            return stars;
+        };
+
+        setStars(createStars());
+    }, []);
+
+    const starComponents = stars.map(star => {
+        const style = {
+            top: `${star.y}rem`,
+            left: `${star.x}rem`,
+            animationDelay: star.delay,
+        };
+
+        return (
+            <div
+                key={star.id}
+                className='scene-stars-universe__star'
+                style={style}
+            />
+        );
+    });
+
+    return (
+        <div className='scene-stars-universe'>
+            {starComponents}
+        </div>
+    );
+}
+
+export { SceneStarsUniverse };
