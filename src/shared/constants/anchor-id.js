346633927a58f30255f1b90a2f51853f4ebd74b3
miniscandal <oscar01dev@gmail.com>
2025-03-08 03:04:18 -0600

    feat: add main header anchor contant and rename footer contant

--- src/shared/constants/anchor-id.js
+++ src/shared/constants/anchor-id.js
@@ -1,16 +1,18 @@
 // These constants are used to refer to specific sections of the page for easy navigation and scrolling
 
+const SCROLL_PAGE_MAIN_HEADER = 'main-header';
 const SCROLL_PAGE_WORK_EXPERIENCE_HISTORY = 'work-experience-history';
 const SCROLL_PAGE_ABOUT_ME = 'about-me';
 const SCROLL_PAGE_PERSONAL_SOFTWARE_PROJECTS = 'personal-software-projects';
 const SCROLL_PAGE_PROFESSIONAL_PROFILE = 'professional-profile';
-const SCROLL_PAGE_PRIMARY_FOOTER = 'primary-footer';
+const SCROLL_PAGE_MAIN_FOOTER = 'main-footer';
 
 
 export {
+    SCROLL_PAGE_MAIN_HEADER,
     SCROLL_PAGE_WORK_EXPERIENCE_HISTORY,
     SCROLL_PAGE_ABOUT_ME,
     SCROLL_PAGE_PERSONAL_SOFTWARE_PROJECTS,
     SCROLL_PAGE_PROFESSIONAL_PROFILE,
-    SCROLL_PAGE_PRIMARY_FOOTER
+    SCROLL_PAGE_MAIN_FOOTER
 };
